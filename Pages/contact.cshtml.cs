2ad2dd05185766108fd0f19e759e25ef4a16e04f
oguzhan.kahyaoglu <oguzhan.kahyaoglu@enerjisauretim.com>
2019-09-17 13:09:05 +0300

    1

---
+++ Pages/contact.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KeycloakTest.Pages
+{
+    [Authorize(Roles = "contact")]
+    public class contact : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
